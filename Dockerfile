0b66eb0eac50f6b3e21f34a406d0c1d3e2cc641f
BARNOCH Anas <anasbarnoch@gmail.com>
2025-12-02 15:49:45 +0100

    Add DockerFile & producer test

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM maven:3.9.4-eclipse-temurin-21 AS builder
+
+WORKDIR /build
+
+COPY pom.xml .
+RUN mvn dependency:go-offline -B
+
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+FROM eclipse-temurin:21-jdk-alpine
+
+WORKDIR /app
+
+COPY --from=builder /build/target/*.jar app.jar
+
+EXPOSE 8099
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/org/brokers/kafkaworkshop/KafkaWorkshopApplication.java
+++ src/main/java/org/brokers/kafkaworkshop/KafkaWorkshopApplication.java
@@ -1,12 +1,46 @@
 package org.brokers.kafkaworkshop;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class KafkaWorkshopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaWorkshopApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        AtomicInteger counter = new AtomicInteger();
+        return args -> {
+            String BROKER_URL = "broker1:9092,broker2:9092,broker3:9092";
+            String TOPIC = "test-topic";
+            String GROUP_ID = "group1";
+
+            Properties props = new Properties();
+            props.put("bootstrap.servers", BROKER_URL);
+            props.put("group.id", GROUP_ID);
+            props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+            props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            KafkaProducer<Integer, String> producer = new KafkaProducer<>(props);
+            Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
+                String msg = String.valueOf(Math.random() * 1000);
+                ProducerRecord<Integer, String> record = new ProducerRecord<Integer, String>(TOPIC, counter.incrementAndGet(), msg);
+                producer.send(record, (recordMetadata, e) -> {
+                    System.out.println("Sending Message Key => " + counter + "Value => " + msg);
+                    System.out.println("Partition => " + recordMetadata.partition() + "Offset => " + recordMetadata.offset());
+                });
+            }, 1000, 1000, TimeUnit.MILLISECONDS);
+        };
     }
 }
