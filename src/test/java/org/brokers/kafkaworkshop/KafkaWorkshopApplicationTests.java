0f66241efa8302fef7b5ac197142ad831fcd4365
BARNOCH Anas <anasbarnoch@gmail.com>
2025-12-02 15:45:44 +0100

    Init

---
+++ src/test/java/org/brokers/kafkaworkshop/KafkaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brokers.kafkaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
